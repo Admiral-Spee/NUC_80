5783ad5501e841255f13263361cb5540fa328aeb
蒋雨轩 <58494892+Admiral-Spee@users.noreply.github.com>
2020-10-15 16:12:07 +0800

    Add files via upload

---
+++ 另类光盘—蒋雨轩.cpp
@@ -0,0 +1,24 @@
+//代码诗《另类光盘》
+//作者：蒋雨轩
+//2020.10.15
+
+#include<iostream>
+#include<stdlib.h>
+
+typedef int Lunch;
+int main()
+{
+    Lunch *Plate;
+    Plate = (Lunch *) malloc (sizeof(Lunch));
+    eat Lunch;
+
+    if(Plate = NULL)
+    {
+        free(Plate);
+        std::cout << "Well done!" << std::endl;
+    }
+    else{
+        std::cout << "Lunch must be finished!" << std::endl;
+    }
+    return empty Plate;
+}
